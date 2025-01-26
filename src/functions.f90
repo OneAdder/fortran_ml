d1e983c51bcf07cb270166c40c1c8e2863225d85
Mikhail Voronov <mikivo@list.ru>
2025-01-26 22:14:23 +0400

    add derivatives for relu, sigmoid and tanh

--- src/functions.f90
+++ src/functions.f90
@@ -3,26 +3,55 @@ module functions
   implicit none
 
 contains
-  function sigmoid(matrix) result(p)
-    real(sp), intent(in) :: matrix(:, :)
-    real(sp) :: p(size(matrix(:, 1)), size(matrix(1, :)))
-    p = 1 / (1 + exp(-matrix))
+  function sigmoid(x) result(y)
+    real(sp), intent(in) :: x(:, :)
+    real(sp) :: y(size(x(:, 1)), size(x(1, :)))
+    y = 1 / (1 + exp(-x))
   end function
   
-  function relu(matrix) result(p)
-    real(sp), intent(in) :: matrix(:, :)
-    real(sp) :: p(size(matrix(:, 1)), size(matrix(1, :)))
+  function sigmoid_derivative(x) result(y)
+    real(sp), intent(in) :: x(:, :)
+    real(sp) :: y(size(x(:, 1)), size(x(1, :)))
+    y = sigmoid(x) * (1 - sigmoid(x))
+  end function
+  
+  function relu(x) result(y)
+    real(sp), intent(in) :: x(:, :)
+    real(sp) :: y(size(x(:, 1)), size(x(1, :)))
     real(sp) :: current
     integer :: i, j
-    do i = 1, size(matrix, 1)
-      do j = 1, size(matrix, 2)
-        current = matrix(i, j)
+    do i = 1, size(x, 1)
+      do j = 1, size(x, 2)
+        current = x(i, j)
         if (current >= 0) then
-          p(i, j) = current
+          y(i, j) = current
         else
-          p(i, j) = 0
+          y(i, j) = 0
         end if
       end do
     end do
+  end function
+
+  function relu_derivative(x) result(y)
+    real(sp), intent(in) :: x(:, :)
+    real(sp) :: y(size(x(:, 1)), size(x(1, :)))
+    real(sp) :: current
+    integer :: i, j
+    do i = 1, size(x, 1)
+      do j = 1, size(x, 2)
+        current = x(i, j)
+        if (current >= 0) then
+          y(i, j) = 1
+        else
+          y(i, j) = 0
+        end if
+      end do
+    end do
+  end function relu_derivative
+
+  function tanh_derivative(x) result(y)
+    real(sp), intent(in) :: x(:, :)
+    real(sp) :: y(size(x(:, 1)), size(x(1, :)))
+    y = 1 - (tanh(x) ** 2)
   end function
 end module
--- tests/test_functions.f90
+++ tests/test_functions.f90
@@ -1,11 +1,20 @@
 program test_functions
   use, intrinsic :: iso_fortran_env, only: sp=>real32, dp=>real64
-  use functions, only: sigmoid, relu
+  use functions, only: sigmoid, sigmoid_derivative, relu, relu_derivative, tanh_derivative
+
   real(sp) :: test_case(2, 3) = reshape([-1.0, -2.0, -3.0, 4.0, 5.0, 6.0], [2, 3])
+
   call test_sigmoid(test_case, reshape(&
       [0.268941432, 0.119202919, 4.74258736E-02,&
       0.982013762, 0.993307173, 0.997527421], [2, 3]))
+  call test_sigmoid_derivative(test_case, reshape(&
+      [0.196611941, 0.104993582, 4.51766588E-02,&
+      1.76627338E-02, 6.64803293E-03, 2.46646581E-03], [2, 3]))
   call test_relu(test_case, reshape([0.0, 0.0, 0.0, 4.0, 5.0, 6.0], [2, 3]))
+  call test_relu_derivative(test_case, reshape([0.0, 0.0, 0.0, 1.0, 1.0, 1.0], [2, 3]))
+  call test_tanh_derivative(test_case, reshape(&
+      [0.419974327, 7.06508160E-02, 9.86599922E-03,&
+      1.34086609E-03, 1.81555748E-04, 2.45571136E-05], [2, 3]))
 
 contains
   subroutine test_sigmoid(m, expected)
@@ -20,6 +29,18 @@ contains
     end if
   end subroutine
 
+  subroutine test_sigmoid_derivative(m, expected)
+    real(sp), intent(in) :: m(:, :)
+    real(sp), intent(in) :: expected(:, :)
+    real(sp) :: actual(size(m(:, 1)), size(m(1, :)))
+    actual = sigmoid_derivative(m)
+    if (all(actual.eq.expected)) then
+      print *, "test_sigmoid_derivative passed"
+    else
+      print *, "test_sigmoid_derivative failed"
+    end if
+  end subroutine
+
   subroutine test_relu(m, expected)
     real(sp), intent(in) :: m(:, :)
     real(sp), intent(in) :: expected(:, :)
@@ -29,6 +50,30 @@ contains
       print *, "test_relu passed"
     else
       print *, "test_relu failed"
+    end if
+  end subroutine
+
+  subroutine test_relu_derivative(m, expected)
+    real(sp), intent(in) :: m(:, :)
+    real(sp), intent(in) :: expected(:, :)
+    real(sp) :: actual(size(m(:, 1)), size(m(1, :)))
+    actual = relu_derivative(m)
+    if (all(actual.eq.expected)) then
+      print *, "test_relu_derivative passed"
+    else
+      print *, "test_relu_derivative failed"
+    end if
+  end subroutine
+
+  subroutine test_tanh_derivative(m, expected)
+    real(sp), intent(in) :: m(:, :)
+    real(sp), intent(in) :: expected(:, :)
+    real(sp) :: actual(size(m(:, 1)), size(m(1, :)))
+    actual = tanh_derivative(m)
+    if (all(actual.eq.expected)) then
+      print *, "test_tanh_derivative passed"
+    else
+      print *, "test_tanh_derivative failed"
     end if
   end subroutine
 end program
