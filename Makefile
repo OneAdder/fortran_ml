c05e0c1d7e7b54b2dcb2852a304a47f907fd3616
Mikhail Voronov <mikivo@list.ru>
2025-01-26 20:57:46 +0400

    update tests

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 COMPILER_TAGS = -g -fcheck=all -Wall
-LIB_FILES = ./src/functions.f90 ./src/linear_regression.f90 ./src/logistic_regression.f90
+LIB_FILES = ./src/functions.f90 ./src/linear_regression.f90 ./src/logistic_regression.f90 ./src/nn.f90
 
 test_regressions:
 	gfortran ${COMPILER_TAGS} ${LIB_FILES} ./tests/test_regressions.f90 -o ./test_regressions
@@ -12,5 +12,10 @@ test_functions:
 	rm ./test_functions
 
 
-test: test_functions test_regressions
+test_layers:
+	gfortran ${COMPILER_TAGS} ${LIB_FILES} ./tests/test_layers.f90 -o ./test_layers
+	./test_layers
+	rm ./test_layers
 
+
+test: test_functions test_regressions test_layers
---
+++ tests/test_layers.f90
@@ -0,0 +1,46 @@
+program test
+  use, intrinsic :: iso_fortran_env, only: sp=>real32, dp=>real64
+  use linear_layer
+
+  real(sp) :: x(4, 3) = reshape([1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0], [4, 3])
+  real(sp) :: grad(2, 3) = reshape([.1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1], [2, 3])
+  print *, "test"
+  call test_linear_layer(x, grad)
+contains
+  subroutine test_linear_layer(x, grad)
+    real(sp) :: x(4, 3)
+    real(sp) :: grad(2, 3)
+    real(sp) :: expected_forward(2, 3) = reshape([0.8, 0.8, 0.8, 0.8, 0.8, 0.8], [2, 3])
+    real(sp) :: expected_grad(4, 3) = reshape(&
+        [0.04, 0.04, 0.04, 0.04, 0.04, 0.04, 0.04, 0.04, 0.04, 0.04, 0.04, 0.04], [4, 3]&
+      )
+    real(sp) :: expected_dw(4, 2) = reshape([0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3], [4, 2])
+    real(sp) :: actual_forward(2, 3)
+    real(sp) :: actual_grad(4, 3)
+    real(sp) :: actual_dw(4, 2)
+    type(LinearLayer) :: linear
+
+    linear = LinearLayer(4, 2)
+    actual_forward = linear%forward(x)
+    actual_grad = linear%backward(x, grad)
+    actual_dw = linear%dw
+
+    if (all(actual_forward.eq.expected_forward)) then
+      print *, "test_linear_layer:forward passed"
+    else
+      print *, "test_linear_layer:forward failed"
+    end if
+    expected_grad = nint(expected_grad * 100)
+    actual_grad = nint(actual_grad * 100)
+    if (all(actual_grad.eq.expected_grad)) then
+      print *, "test_linear_layer:backward passed"
+    else
+      print *, "test_linear_layer:backward failed"
+    end if
+    if (all(actual_dw.eq.expected_dw)) then
+      print *, "test_linear_layer:dw passed"
+    else
+      print *, "test_linear_layer:dw failed"
+    end if
+  end subroutine
+end program
